0babb8b1c2a1a38a3613ec1c7ea896b14fcaa753
sujitha <sundarsujitha87@gmail.com>
2023-01-20 14:18:52 +0530

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Header(){
+    return(
+        <nav className="App-header">
+            <img src="logo192.png" alt="logo" />
+            <h3>
+               React
+            </h3>
+            <h4>
+                <a
+                    className="App-footer"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                Learn React
+                </a>
+            </h4>
+        </nav>
+    )
+}
+
+export default Header;
---
+++ src/Main.js
@@ -0,0 +1,13 @@
+function Main(){
+    return(
+        <main className="App-main">
+            <h1>A JavaScript library for building user interfaces</h1>
+            <ul >
+                <li>Declarative</li>
+                <li>Component-Based</li>
+                <li>Learn Once, Write Anywhere</li>
+            </ul>
+        </main>
+    )
+}
+export default Main;
